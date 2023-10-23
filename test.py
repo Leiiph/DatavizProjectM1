09a39702938e4817e4f48a3eae1b0070f4f106db
Leiiph <60736065+Leiiph@users.noreply.github.com>
2023-10-23 18:42:25 +0200

    Added picture file + started the streamlit app
    Also removed data2

---
+++ test.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title('DataViz Project: About Electricity and Gaz consumption in France')
+st.sidebar.image("frig.jpg", width=200, caption="picture of a frog")
+st.sidebar.write("Hello! I am Lena, a M1 Data Engineering student!")
+st.sidebar.write("You can find my work and me here:")
+st.sidebar.write("[Github](https://github.com/Leiiph)")
+st.sidebar.write("[Linkedin](https://www.linkedin.com/in/l%C3%A9nabrunier/)")
