5d3a169a7d4422d47d640ee1f655036faebccb94
Leiiph <60736065+Leiiph@users.noreply.github.com>
2023-10-27 00:12:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import geodatasets as godt
 import geopandas as gop
 import plotly.express as px
